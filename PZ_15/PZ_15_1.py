6ced0ccef1f70dc4851f18a8827e22d85e7cde86
Vladimir <73241722+E-bait@users.noreply.github.com>
2022-03-21 00:42:59 +0300

    1

---
+++ PZ_15/PZ_15_1.py
@@ -0,0 +1,18 @@
+# v 23
+# В матрице найти максимальный положительный элемент, кратный 4.
+
+import random
+N = int(input("Введите размер матрицы:"))
+arr = [[random.randint(1, 9) for i in range(N)] for j in range(N)]  # матрица
+m = 0
+
+for i in range(N):
+    for j in range(N):
+        if (arr[i][j] % 4 == 0) and arr[i][j] > m:                 # находим максимальное число кратное 4
+            m = arr[i][j]
+
+
+for i in range(N):                                             # выводим матрицу
+    print(arr[i])
+
+print(m)
